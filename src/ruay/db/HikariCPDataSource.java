ba2605424c470602f1513584d865036525446d47
PoN <pon_7777@hotmail.com>
2020-04-21 14:19:28 +0700

    commit

---
+++ src/ruay/db/HikariCPDataSource.java
@@ -0,0 +1,39 @@
+package ruay.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import ruay.db.config.DatabaseConfig;
+
+@Component
+public class HikariCPDataSource {
+    private static final Logger LOG = LoggerFactory.getLogger(HikariCPDataSource.class);
+    
+    public static HikariDataSource ds = null;
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+    
+    @PostConstruct
+    public void init() {
+	    HikariConfig config = new DatabaseConfig().getHikariConfig();
+	    ds = new HikariDataSource(config);
+	    LOG.info("HikariDataSource Started");
+    }
+    
+    @PreDestroy
+    public void shutDown() {
+	    ds.close();
+    }
+}
---
+++ src/ruay/db/config/DatabaseConfig.java
@@ -0,0 +1,30 @@
+package ruay.db.config;
+
+import com.zaxxer.hikari.HikariConfig;
+
+public class DatabaseConfig {
+	private final String DB_SERVER = "localhost";
+	private final String DB_PORT = "3306";
+	private final String DB_NAME = "ruaydb";
+	private final String DB_USERNAME = "root";
+	private final String DB_PASSWORD = "1234";
+
+	public HikariConfig getHikariConfig() {
+		HikariConfig config = new HikariConfig();
+		config.setLeakDetectionThreshold(3600000); // 1 hour
+		config.setMinimumIdle(5);
+		config.setMaximumPoolSize(10);
+		config.setConnectionTestQuery("/* ping */ SELECT 1");
+		config.setConnectionTimeout(30000); // 30 sec
+		config.setMaxLifetime(1800000); // 30 mins
+		config.setIdleTimeout(300000); // 5 mins
+		config.setPoolName("RUAY-HikariPool-1");
+		config.setDataSourceClassName("org.mariadb.jdbc.MariaDbDataSource");
+		config.addDataSourceProperty("serverName", DB_SERVER);
+		config.addDataSourceProperty("portNumber", DB_PORT);
+		config.addDataSourceProperty("user", DB_USERNAME);
+		config.addDataSourceProperty("password", DB_PASSWORD);
+		config.addDataSourceProperty("databaseName", DB_NAME);
+		return config;
+	}
+}
